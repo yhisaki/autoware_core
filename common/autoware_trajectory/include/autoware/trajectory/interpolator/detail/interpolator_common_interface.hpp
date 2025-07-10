fe12891b3da43f16c6d3c32a5e5d34f73336c82c
Yukinari Hisaki <42021302+yhisaki@users.noreply.github.com>
2025-07-10 15:34:39 +0900

    Update interpolator_common_interface.hpp

--- common/autoware_trajectory/include/autoware/trajectory/interpolator/detail/interpolator_common_interface.hpp
+++ common/autoware_trajectory/include/autoware/trajectory/interpolator/detail/interpolator_common_interface.hpp
@@ -120,8 +120,8 @@ protected:
       return static_cast<int32_t>(bases_.size()) - 2;
     }
     auto comp = [](const double & a, const double & b) { return a <= b; };
-    return std::distance(bases_.begin(), std::lower_bound(bases_.begin(), bases_.end(), s, comp)) -
-           1;
+    const int32_t idx = std::distance(bases_.begin(), std::lower_bound(bases_.begin(), bases_.end(), s, comp)) - 1;
+    return std::clamp(idx, 0, base.size() - 1);
   }
 
 public:
